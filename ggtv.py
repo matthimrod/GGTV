fbfff07ddb58952c5d21b7156e5e0b413f8f6350
Matt Himrod <matt@matthimrod.com>
2020-08-23 18:08:09 -0400

    Initial Release

--- ggtv.py
+++ ggtv.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import http.server
-import mimetypes
 import os
 import pychromecast
 import random
@@ -11,10 +10,12 @@ import time
 import urllib.parse
 
 DIRNAME = '/home/matthimrod/GGTV/'
-BASEURL = 'http://192.168.86.45:8000/'
 PORT = 8000
-RECEIVER = 'Bing-TV'
+RECEIVER = 'Outside TV'
 
+ip = os.popen("hostname -I | cut -d' ' -f1").read().strip()
+
+BASEURL = 'http://' + ip + ':' + str(PORT) + '/'
 
 def getListOfFiles(dirName):
     listOfFiles = list()
@@ -28,12 +29,16 @@ def start_server():
     Handler = http.server.SimpleHTTPRequestHandler
 
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
-        print("serving at port", PORT)
+        print("Starting HTTP server on port", PORT)
         httpd.serve_forever()
 
 def main():
+    print("Gathering list of files.")
     listOfFiles = getListOfFiles(DIRNAME)
+    print("Found ", len(listOfFiles), " files.")
     random.shuffle(listOfFiles)
+
+    print("Base URL: ", BASEURL)
 
     daemon = threading.Thread(name='daemon_server',
                               target=start_server,
@@ -46,24 +51,26 @@ def main():
     chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[RECEIVER])
     cast = chromecasts[0]
     cast.wait()
-    print("Using Chromecast:")
-    print(cast.device)
+    print("Using Chromecast:", cast.device.friendly_name)
 
     for elem in listOfFiles:
-        print("Starting file:", elem)
-        mime = mimetypes.guess_type(elem)
-        mime = "video/mp4"
-        url = BASEURL + urllib.parse.quote(elem[len(DIRNAME):], safe='/')
-        print("Sending URL:", url)
+        time.sleep(2)
 
-        time.sleep(1)
-        cast.media_controller.play_media(url , mime)
-        time.sleep(5)
+        print("Starting file:", elem)
+        url = BASEURL + urllib.parse.quote(elem[len(DIRNAME):], safe='/')
+
+        print("Sending URL:", url)
+        cast.media_controller.play_media(url, 'video/mp4')
+
+        time.sleep(10)
+
+        cast.media_controller.block_until_active(15)
         cast.media_controller.update_status()
-#        cast.media_controller.block_until_active()
-        print(cast.media_controller.status)
-        while(cast.media_controller.is_playing or cast.media_controller.is_paused):
-            time.sleep(15)
+        print('Playing ', cast.media_controller.status.content_id,
+                  ' (', cast.media_controller.status.duration, ')')
+        while(cast.media_controller.is_playing or 
+              cast.media_controller.is_paused):
+            time.sleep(5)
     
 
 if __name__ == '__main__':
